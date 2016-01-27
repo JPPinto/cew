27a3a330bc685bdfe65d6bd4e5b8cfeaa692d2ab
jpmmaia <jpmmaia@gmail.com>
2016-01-27 14:29:47 +0000

    Refactoring.

---
+++ js/map.js
@@ -0,0 +1,22 @@
+function initializeMap()
+{
+    var location = new google.maps.LatLng(41.178008, -8.605976);
+
+    var map = new google.maps.Map(
+        document.getElementById("map-container"),
+        {
+            zoom: 15,
+            center: location
+        }
+    );
+
+    var marker = new google.maps.Marker(
+        {
+            position: location,
+            map: map,
+            title: 'CISTER Research Center'
+        }
+    );
+}
+
+google.maps.event.addDomListener(window, 'load', initializeMap);
